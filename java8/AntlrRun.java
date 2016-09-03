4b78c2986877015aaca710185007b62aec7f9a14
Thomas <ThomasBJones2@gmail.com>
2016-09-03 16:45:23 -0700

    Found the method for the parsers.

---
+++ java8/AntlrRun.java
@@ -0,0 +1,13 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class AntlrRun {
+    public static void main(String[] args) throws Exception {
+        Java8Lexer lexer = new Java8Lexer(new ANTLRFileStream("./examples/helloworld.java"));
+        Java8Parser parser = new Java8Parser(new CommonTokenStream(lexer));
+        System.out.println(parser.literal());
+	     //ParseTree tree = parser.literal();
+    	  //ParseTreeWalker walker = new ParseTreeWalker();
+        //walker.walk( new Java8Walker(), tree );
+    }
+}
