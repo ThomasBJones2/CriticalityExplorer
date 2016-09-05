a7dd9f9128a23ee33c2b3dee473a034a1ceedee5
Thomas <ThomasBJones2@gmail.com>
2016-09-04 20:21:55 -0700

    Found the head of the java8 parser

--- java8/AntlrRun.java
+++ java8/AntlrRun.java
@@ -5,9 +5,10 @@ public class AntlrRun {
     public static void main(String[] args) throws Exception {
         Java8Lexer lexer = new Java8Lexer(new ANTLRFileStream("./examples/helloworld.java"));
         Java8Parser parser = new Java8Parser(new CommonTokenStream(lexer));
-        System.out.println(parser.literal());
-	     //ParseTree tree = parser.literal();
-    	  //ParseTreeWalker walker = new ParseTreeWalker();
-        //walker.walk( new Java8Walker(), tree );
+        //System.out.println(parser.classDeclaration());
+	     ParseTree tree = parser.compilationUnit();
+		  //System.out.println(tree.toStringTree());
+    	  ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk( new Java8Walker(), tree );
     }
 }
