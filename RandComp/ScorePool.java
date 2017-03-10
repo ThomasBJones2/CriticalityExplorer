bb7a2e08b8cb2f8a67ed85508b9f96db5066ec3b
Thomas Jones <tj@hyperthesis.co>
2017-03-10 07:08:20 -0800

    fixing the +1 problem with multiply logarithm and absolute percentage error

--- RandComp/ScorePool.java
+++ RandComp/ScorePool.java
@@ -10,36 +10,31 @@ public class ScorePool{
 
 		public static Score absolutePercentValue(double error, double correct){
 			
-			return new Score(correct > 0?
-			Math.abs(error - correct)/correct:0, 
+			return new Score(Math.abs(error - correct)/Math.abs(correct + 1), 
 			"Absolute_Percent_Value"); 
 		}
 
 		public static Score absoluteValueBigInteger(BigInteger error, BigInteger correct){ 
-			if(error != null && correct != null) {
-				double score = error.subtract(correct).abs().doubleValue();
-				return new Score(score, 
-					"Absolute_Value"); 
-			} else {
-				return new Score(0, "Absolute_Value");
-			}
+			BigInteger newError = (error == null)?BigInteger.ZERO:error;
+			BigInteger newCorrect = (correct == null)?BigInteger.ZERO:correct);
+			double score = error.subtract(correct).abs().doubleValue();
+			return new Score(score, "Absolute_Value"); 
 		}
 
 		public static Score logarithmAbsoluteValueBigInteger(BigInteger error, BigInteger correct){ 
-			
-			if (error != null && correct != null) {
-				return new Score(error.subtract(correct).abs().doubleValue() != 0?
-					Math.log((error.subtract(correct)).abs().doubleValue()):0, 
-					"Absolute_Logarithm_Value"); 
-			} else {
-				return new Score(0, "Absolute_Logarithm_Value");
-			}
+			BigInteger newError = (error == null)?BigInteger.ZERO:error;
+			BigInteger newCorrect = (correct == null)?BigInteger.ZERO:correct);
+			return new Score(
+				Math.log((error.subtract(correct).add(BigInteger.ONE)).abs().doubleValue()), 
+				"Absolute_Logarithm_Value"); 
 		}
 
 
 		public static Score absolutePercentValueBigInteger(BigInteger error, BigInteger correct){
-			return new Score(correct.doubleValue() != 0?
-			(((error.subtract(correct)).abs()).divide(correct.abs())).doubleValue():0, 
+			BigInteger newError = (error == null)?BigInteger.ZERO:error;
+			BigInteger newCorrect = (correct == null)?BigInteger.ZERO:correct);
+			return new Score(
+			(((error.subtract(correct)).abs()).divide(correct.abs().add(BigInteger.ONE))).doubleValue(), 
 			"Absolute_Percent_Value"); 
 		}
 
